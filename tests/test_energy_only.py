78c7095da8a04d55f27db7b0b467eee0f257c184
Tejas <tejasdahiya0007@gmail.com>
2025-11-01 21:47:53 -0500

    chore: remove test directory (cleanup for optimization-only release)

--- tests/test_energy_only.py
+++
@@ -1,8 +0,0 @@
-from ase import Atoms
-from anisoap_ase import AniSOAPCalculator
-
-def test_energy_constant():
-    def efn(a: Atoms) -> float: return 0.5
-    a = Atoms("He", positions=[[0,0,0]])
-    a.calc = AniSOAPCalculator(energy_fn=efn)
-    assert abs(a.get_potential_energy() - 0.5) < 1e-12
